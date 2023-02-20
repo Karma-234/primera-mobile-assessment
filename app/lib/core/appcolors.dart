d0df166ecd43a24453d14b0d52f9c1aab8f6251c
kamal Busari <busari1kamal@gmail.com>
2023-02-20 20:13:13 +0800

    WIP: First commit

---
+++ app/lib/core/appcolors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+
+class Appcolors {
+  static const Color bg80 = Color(0xFF202028);
+  static const Color green100 = Color(0xFF00A85A);
+  static const Color text80 = Color(0xFFECECEE);
+  static const Color text100 = Color(0xFF02060E);
+  static const Color primary100 = Color(0xFF0C2C60);
+  static const Color inactive = Color(0xFF959499);
+  static const Color neutral = Color(0xFFFFFFFF);
+  static const Color def = Color(0xFF959499);
+  static const Color text50 = Color(0xFF959499);
+  static const Color blackOut = Colors.black;
+}
---
+++ app/lib/presentation/more_view.dart
@@ -0,0 +1,15 @@
+import 'package:app/core/appcolors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MoreView extends StatelessWidget {
+  const MoreView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Appcolors.def,
+    );
+  }
+}
